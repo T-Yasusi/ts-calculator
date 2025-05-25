e3c7e145a6a3d5de4471864e13774d6340cd235f
T. Yasusi <yasusi.844@gmail.com>
2025-05-25 22:29:55 +0900

    テスト用JSディレクトリtest-js/に変更

--- temp/operator.ts
+++
@@ -1,8 +0,0 @@
-import { add } from '../modules/operators.js';
-import { Complex } from '../modules/Complex.js';
-const x1 = 0;
-const x2 = 1;
-const z1 = new Complex(1, 1);
-const z2 = new Complex(0, 1);
-console.log(add(x1, x2));
-console.log(add(z1, z2));
