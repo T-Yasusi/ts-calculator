4c3d221d6ac9e46665cfaddb9c11e7fb0bf3d4e7
T. Yasusi <yasusi.844@gmail.com>
2025-05-25 22:33:46 +0900

    Updates

---
+++ test/operator.ts
@@ -0,0 +1,11 @@
+import { add } from '../modules/operators.js'
+import { Complex } from '../modules/Complex.js'
+
+const x1=0;
+const x2=1;
+
+const z1=new Complex(1, 1);
+const z2=new Complex(0, 1);
+
+console.log(x1+x2);
+console.log(z1+z2);
